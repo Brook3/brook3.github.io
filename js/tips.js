e56255c155e4b3bc4d8c7b764942ce4fb1c099ea
Brook3 <brook3@qq.com>
2021-08-08 21:11:43 +0800

    整体用terminal显示

--- js/tips.js
+++ js/tips.js
@@ -4,10 +4,11 @@ $(function(){
         '爱，就大声说出来，因为你永远都不会知道，明天和意外，哪个会先来！',
     ];
     // 定时更改提示信息
-    $("#dwm_tips .tips_content").text(tips_list[0]);
+    var obj_tips = $("#dwm_tips .tips_content,#dwm_terminal .content_tips .content_text");
+    obj_tips.text(tips_list[0]);
     var index = 1;
     setInterval(function () {
-        $("#dwm_tips .tips_content").text(tips_list[index]);
+        obj_tips.text(tips_list[index]);
         index ++;
         if (index > tips_list.length-1) {
             index = 0;
